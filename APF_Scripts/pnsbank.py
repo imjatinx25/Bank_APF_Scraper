6b886b508921bcdc2eadd8c305fdee908d6053de
Jatin Gautam <jatin.gautam@basichomeloan.com>
2026-01-07 16:18:16 +0530

    fixing chrome sandbox env

--- APF_Scripts/pnsbank.py
+++ APF_Scripts/pnsbank.py
@@ -15,8 +15,10 @@ from collections import defaultdict
 
 def initialize_driver():
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
     return webdriver.Chrome(options=options)
 
