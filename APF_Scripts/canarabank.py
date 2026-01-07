a7e0fe75aa9c92de36d35b5e791ec3d717b279f3
Jatin Gautam <jatin.gautam@basichomeloan.com>
2026-01-07 14:33:35 +0530

    fixing chrome sandbox crash in doc env

--- APF_Scripts/canarabank.py
+++ APF_Scripts/canarabank.py
@@ -17,6 +17,8 @@ def initialize_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
     return webdriver.Chrome(options=options)
 
